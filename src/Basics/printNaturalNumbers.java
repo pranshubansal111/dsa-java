892b8a04995f378b3dae3cd9612c46b6624e0ae2
pranshubansal111 <pranshubansal111@gmail.com>
2025-05-01 17:13:29 +0530

    commiting java code

---
+++ src/Basics/printNaturalNumbers.java
@@ -0,0 +1,16 @@
+package Basics;
+
+import java.util.Scanner;
+
+// code to print 'n' natural numbers
+
+public class printNaturalNumbers{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter numbers upto which you want to print: ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
