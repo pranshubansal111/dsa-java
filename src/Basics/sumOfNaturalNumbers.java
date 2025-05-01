4a2ed478eb11a0cc4e14358bd8e116534f945e04
pranshubansal111 <pranshubansal111@gmail.com>
2025-05-01 17:17:33 +0530

    commiting java code

---
+++ src/Basics/sumOfNaturalNumbers.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+//Code to print sum of n natural numbers
+public class sumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of terms - ");
+        int n = sc.nextInt();
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            sum += i;
+        }
+        System.out.println("Sum of natural numbers = " + sum);
+    }
+}
